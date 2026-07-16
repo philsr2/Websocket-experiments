c6f368d352c1f183badcf3aa70136e0788bc0e8f
Phil Hartfield I <philsr2@gmail.com>
2026-07-16 05:58:40 -0500

    Update chat-server.py
    Fixed my comment characters and a couple of long lines that broke when I pasted in the code.

--- websockets-chat/chat-server.py
+++ websockets-chat/chat-server.py
@@ -3,7 +3,7 @@ import websockets
 import json
 import signal
 import itertools
-// import ssl
+# import ssl
 
 CLIENTS = set()
 id_counter = itertools.count(1)
@@ -23,11 +23,9 @@ async def handler(websocket):
                 print(f"new nick is {message[6:]}")
                 old_id=client_ids[websocket]
                 client_ids[websocket]=message[6:]
-                await websocket.send(make_msg("status",text=f"nickname changed t
-o {message[6:]}"))
+                await websocket.send(make_msg("status",text=f"nickname changed to {message[6:]}"))
                 others = CLIENTS - {websocket}
-                data=make_msg("status",text=f"{old_id} changed nickname to {clie
-nt_ids[websocket]}")
+                data=make_msg("status",text=f"{old_id} changed nickname to {client_ids[websocket]}")
                 websockets.broadcast(others, data)
                 pass
             await websocket.send(make_msg("echo", text=f"{message}"))
@@ -42,22 +40,22 @@ nt_ids[websocket]}")
         CLIENTS.discard(websocket)
         del client_ids[websocket]
 async def main():
-//    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-//    ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
-//
-//    i took the cert that I got from Let's Encrypt with my https server
-//    split the secret key into the key.pem file and left the cert/ca in cert.pem
-//
-//    there is an openssl command to read the pem files, found it on google...
-//    if you want to verify the contents =)
+#    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+#    ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+#
+#    i took the cert that I got from Let's Encrypt with my https server
+#    split the secret key into the key.pem file and left the cert/ca in cert.pem
+#
+#    there is an openssl command to read the pem files, found it on google...
+#    if you want to verify the contents =)
 
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
     ip="localhost"
-//    async with websockets.serve(handler, ip, 8443,ssl=ssl_context):
-//        print(f"Server running on wss://{ip}:8443")
+#    async with websockets.serve(handler, ip, 8443,ssl=ssl_context):
+#        print(f"Server running on wss://{ip}:8443")
     async with websockets.serve(handler, ip, 8765):
         print(f"Server running on wss://{ip}:8765")
         await stop  # Run until SIGTERM
